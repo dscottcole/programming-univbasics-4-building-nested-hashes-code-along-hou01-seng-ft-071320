7fca27b0a4d1eed14edfbecf24be05fdc2671eab
Daniel Cole <danielscole1@gmail.com>
2020-07-01 17:32:04 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -3,16 +3,16 @@ def first_challenge
      montague: {:patriarch => {
       :name => "Lord Montague",
       :age => "53"
-  },
-    :matriarch => {
+      },
+      :matriarch => {
       :name => "Lady Montague",
       :age => "54"
-  },
-    :hero => {
+      },
+      :hero => {
       :name => "Romeo",
       :age => "15"
-  },
-    :hero_friends => [
+      },
+      :hero_friends => [
       {
          :name => "Benvolio",
          :age => "17",
@@ -23,7 +23,32 @@ def first_challenge
         :age => "18",
         :attitude => "hot-headed"
       }
-    ]}
-     capulet: {}
-   }
+      ]},
+     capulet: {
+      :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+      },
+      :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+      },
+      :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+      },
+      :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
 end
